5050725ce13b76d01d0a08a3bce3d65f5075779a
Kevin Cuadros <kev040798@gmail.com>
2020-08-24 21:47:33 -0400

    Create attributes test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,12 +2,20 @@
 
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'ship.rb'
+require './lib/ship.rb'
 
 class ShipTest < Minitest::Test
   def test_it_exists
     ship = Ship.new('Cruiser', 3)
 
     assert_instance_of Ship, ship
+  end
+
+  def test_attributes_exist
+    ship = Ship.new('Cruiser', 3) 
+
+    assert_equal "Cruiser", ship.name
+    assert_equal 3, ship.length
+    assert_equal 3, ship.health
   end
 end
