d888f9f11c895791947690b8fa59b8f814a5a707
Kevin Cuadros <kev040798@gmail.com>
2020-08-26 18:29:46 -0400

    Created cells hash

--- lib/board.rb
+++ lib/board.rb
@@ -5,7 +5,23 @@ require './lib/cell'
 
 # Displays the game board
 class Board
-  def initialize
-    @cells = {}
+  attr_reader :cells
+  def initialize(board_size = 4)
+    @board_size = board_size
+    @cells = create_cells
+  end
+
+  def create_cells
+    cells = {}
+    letters = (65..(@board_size + 64)).map(&:chr)
+    numbers = (1..@board_size)
+
+    letters.each do |letter|
+      numbers.each do |number|
+        position = letter + number.to_s
+        cells[position] = Cell.new(position)
+      end
+    end
+    cells
   end
 end
