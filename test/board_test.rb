0011dcc4c9c9c9fb9cebf140362898fd027cf0b1
Aidan Murray <aidanhansm@gmail.com>
2020-08-26 16:10:37 -0600

    Make test for cells more comprehensive

--- test/board_test.rb
+++ test/board_test.rb
@@ -12,8 +12,14 @@ class BoardTest < Minitest::Test
   end
 
   def test_cells
-    board = Board.new
+    board = Board.new(4)
 
+    assert_instance_of Hash, board.cells
     assert_equal 16, board.cells.length
+
+    board.cells.each do |position, cell|
+      assert_instance_of String, position
+      assert_instance_of Cell, cell
+    end
   end
 end
