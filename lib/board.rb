814d4d8bf8ece0f19043c76f82047ad512186fe6
Aidan Murray <aidanhansm@gmail.com>
2020-08-26 19:02:35 -0600

    Add logic for testing for consecutive coords

--- lib/board.rb
+++ lib/board.rb
@@ -27,5 +27,14 @@ class Board
 
   def valid_placement?(ship, coordinates)
     return false unless ship.length == coordinates.length
+
+    first_coords = coordinates[0].split('')
+
+    coordinates.each_with_index do |coords, idx|
+      split_coords = coords.split('')
+      unless first_coords[0].ord + idx == split_coords[0].ord || first_coords[1].to_i + idx == split_coords[1].to_i
+        return false
+      end
+    end
   end
 end
