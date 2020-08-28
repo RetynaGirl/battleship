27c29eb9f1d83324936344d89bd2181fc581d8fb
Kevin Cuadros <kev040798@gmail.com>
2020-08-28 18:47:05 -0400

    Create test for player places ship

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,6 +5,7 @@ require 'minitest/pride'
 require './lib/board'
 require './lib/player'
 
+
 class PlayerTest < Minitest::Test
   def test_it_exists
     player = Player.new
@@ -16,5 +17,20 @@ class PlayerTest < Minitest::Test
     player = Player.new
 
     assert_equal 2, player.generate_player_ships.length
+  end
+
+  def test_player_places_ship
+    player1 = Player.new
+    ship1 = Ship.new('Cruiser', 3)
+
+    player1.player_place_ship(ship1, %w[A1 A2 A3])
+
+    actual1 = player1.player.cells['A1'].ship
+    actual2 = player1.player.cells['A1'].ship
+    actual3 = player1.player.cells['A1'].ship
+
+    assert_equal ship1, actual1
+    assert_equal ship1, actual2
+    assert_equal ship1, actual3
   end
 end
