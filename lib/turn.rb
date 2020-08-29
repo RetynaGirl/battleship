cce3462cb30986e3b6a164dc5991bcb89f2e8f9f
Kevin Cuadros <kev040798@gmail.com>
2020-08-29 19:48:38 -0400

    Add display board and player shot method

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,6 +3,25 @@
 
 # Turn class
 class Turn
+  attr_reader :player, :computer
   def initialize(player, computer)
+    @player = player
+    @computer = computer
+  end
+
+  def display_boards
+    @player.board.render(true)
+    @computer.board.render
+  end
+
+  def player_shot_prompt
+    p 'Enter the coordinate for your shot:'
+    coordinate = gets.chomp.upcase
+
+    until @player.board.valid_coordinate?(coordinate)
+      p 'Please enter a valid coordinate:'
+      coordinate = gets.chomp.upcase
+    end
+    coordinate
   end
 end
