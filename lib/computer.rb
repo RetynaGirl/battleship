bc2f5a577abf35b0159ab6d09f0e4ba504a7e74d
Aidan Murray <aidanhansm@gmail.com>
2020-08-28 22:44:47 -0600

    Add generate ships method

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
-class Computer
+require './lib/ship'
 
+# Holds data and helpful methods for the Computer player
+class Computer
+  attr_reader :ships
+
+  def initialize(ship_names = { 'Submarine' => 2, 'Cruiser' => 3 })
+    @ships = generate_ships(ship_names)
+  end
+
+  def generate_ships(ship_names)
+    ship_names.map do |name, length|
+      Ship.new(name, length)
+    end
+  end
 end
