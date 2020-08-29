dfdd3265ea32576a2044044f85aa7f7f743a78d2
Aidan Murray <aidanhansm@gmail.com>
2020-08-28 22:44:28 -0600

    Add test for generate ships

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -10,4 +10,14 @@ class ComputerTest < Minitest::Test
 
     assert_instance_of Computer, computer
   end
+
+  def test_generate_default_ships
+    # Throws that failure that means it actually worked
+
+    computer = Computer.new
+    ship1 = Ship.new('Submarine', 2)
+    ship2 = Ship.new('Cruiser', 3)
+
+    assert_equal [ship1, ship2], computer.ships
+  end
 end
