a1679e89abf02c72f483d24b9e4fc610e9c011c2
Aidan Murray <aidanhansm@gmail.com>
2020-08-29 12:56:54 -0600

    Add place_ships and generate_coords methods
    place_ships cannot be tested yet

--- lib/computer.rb
+++ lib/computer.rb
@@ -17,4 +17,26 @@ class Computer
       Ship.new(name, length)
     end
   end
+
+  def place_ships
+    @ships.each do |ship|
+      direction = %i[horizontal vertical].sample
+      coord = @board.cells.keys.sample
+
+      coordinates = generate_coords(ship, direction, coord)
+    end
+  end
+
+  def generate_coords(ship, direction, coord)
+    coord_data = coord.split('')
+
+    (1..ship.length).map do |num|
+      case direction
+      when :horizontal
+        "#{coord_data[0]}#{(coord_data[1].to_i + (num - 1))}"
+      when :vertical
+        "#{(coord_data[0].ord + (num - 1)).chr}#{coord_data[1]}"
+      end
+    end
+  end
 end
