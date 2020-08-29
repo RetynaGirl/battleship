53effa4804a96be79d85240c037205e54cb61f71
Kevin Cuadros <kev040798@gmail.com>
2020-08-29 18:23:31 -0400

    Test for lost

--- test/player_test.rb
+++ test/player_test.rb
@@ -20,14 +20,14 @@ class PlayerTest < Minitest::Test
   end
 
   def test_player_places_ship
-    player1 = Player.new
+    player = Player.new
     ship1 = Ship.new('Cruiser', 3)
 
-    player1.player_place_ship(ship1, %w[A1 A2 A3])
+    player.player_place_ship(ship1, %w[A1 A2 A3])
 
-    actual1 = player1.board.cells['A1'].ship
-    actual2 = player1.board.cells['A2'].ship
-    actual3 = player1.board.cells['A3'].ship
+    actual1 = player.board.cells['A1'].ship
+    actual2 = player.board.cells['A2'].ship
+    actual3 = player.board.cells['A3'].ship
 
     assert_equal ship1, actual1
     assert_equal ship1, actual2
@@ -35,11 +35,28 @@ class PlayerTest < Minitest::Test
   end
 
   def test_valid_sequence?
-    player1 = Player.new
+    player = Player.new
     ship1 = Ship.new('Cruiser', 3)
     ship2 = Ship.new('Cruiser', 3)
 
-    assert_equal true, player1.valid_sequence?(ship1, %w[A1 A2 A3])
-    assert_equal false, player1.valid_sequence?(ship2, %w[A3 A1 A2])
+    assert_equal true, player.valid_sequence?(ship1, %w[A1 A2 A3])
+    assert_equal false, player.valid_sequence?(ship2, %w[A3 A1 A2])
+  end
+
+  def test_player_lost
+    player = Player.new
+
+    assert_equal false, player.lost?
+
+    player.ships[0].hit
+    player.ships[0].hit
+
+    assert_equal false, player.lost?
+
+    player.ships[1].hit
+    player.ships[1].hit
+    player.ships[1].hit
+
+    assert_equal true, player.lost?
   end
 end
