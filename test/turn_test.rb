7c3e03c0f5e8b39be9871ad71a35c9a3cdba3e93
Kevin Cuadros <kev040798@gmail.com>
2020-08-29 18:41:01 -0400

    Fix test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,9 +4,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
 require './lib/player'
+require './lib/turn'
+require './lib/computer'
 
 class TurnTest < Minitest::Test
   def test_it_exists
+    player = Player.new
+    computer = Computer.new
     turn = Turn.new(player, computer)
 
     assert_instance_of Turn, turn
