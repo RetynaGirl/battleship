a33ab9040fd4454a4bd50050fb4fa67a4f7e7623
Aidan Murray <aidanhansm@gmail.com>
2020-08-30 12:17:17 -0600

    Add game class and initialize

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Runs the game with a player and a computer
+class Game
+  def initialize(player, computer)
+    @player = player
+    @computer = computer
+  end
+end
