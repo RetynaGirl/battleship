b6b92353d1ffe010616e044370974d1bbd7c6e25
Aidan Murray <aidanhansm@gmail.com>
2020-08-30 13:12:22 -0600

    Remove unnecessary arguments from game exists test

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,10 +8,7 @@ require './lib/computer'
 
 class GameTest < Minitest::Test
   def test_it_exists
-    player = Player.new
-    computer = Computer.new
-
-    game = Game.new(player, computer)
+    game = Game.new
 
     assert_instance_of Game, game
   end
