e2fa52cc25d9c1c24903715eeb684b72339b1912
Aidan Murray <aidanhansm@gmail.com>
2020-08-30 13:15:59 -0600

    Add

--- lib/game.rb
+++ lib/game.rb
@@ -30,4 +30,22 @@ class Game
     @computer.place_ships
     player_place_ships
   end
+
+  def player_place_ships
+    p 'I have laid out my ships on the grid.'
+    p "You now need to lay out your #{@ships.length} ships"
+    p "The #{@ships.map.with_index do |ship, idx|
+               "#{ship[0]} is #{ship[1]} units long#{', and the ' if @ships.length > (idx + 1)}"
+             end.join}."
+
+    @player.ships.each do |ship|
+      p @player.board.render(true)
+      p "Enter the squares for the #{ship.name} (#{ship.length} spaces):\n> "
+      coordinates = gets.chomp.upcase.split(' ')
+      until @player.place_ship(coordinates)
+        p "Those are invalid coordinates. Please try again:\n> "
+        coordinates = gets.chomp.upcase.split(' ')
+      end
+    end
+  end
 end
